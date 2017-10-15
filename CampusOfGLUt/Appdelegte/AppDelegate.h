60ae3e10213705a32054c8e7007996f05955de94
何天从 <ihetiancong@efun.com>
2017-10-15 12:08:09 +0800

    update version 3.0.0

---
+++ CampusOfGLUt/Appdelegte/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  CampusOfGLUt
+//
+//  Created by HTC on 15/2/11.
+//  Copyright (c) 2015年 HTC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
